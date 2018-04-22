46e9a929f84da0059110a35b4a8c2663b2e81520
Brendon <brendonwbw@gmail.com>
2018-04-21 19:39:34 -0700

    Integrate Google Streetview API

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 
 function loadData() {
 
+    // Dollar signs are merely used to indicate that an object is a jQuery object
     var $body = $('body');
     var $wikiElem = $('#wikipedia-links');
     var $nytHeaderElem = $('#nytimes-header');
@@ -11,9 +12,25 @@ function loadData() {
     $wikiElem.text("");
     $nytElem.text("");
 
-    // load streetview
-
-    // YOUR CODE GOES HERE!
+    // create Google Streetview request url from user inputted street and city,
+    // then append an img tag with the request url to the HTML body
+    var street = $("#street").val();
+    var city = $("#city").val();
+    var google_request_url = "http://maps.googleapis.com/maps/api/streetview?size=12000x600&location=" + street + ", " + city;
+    $body.append('<img class="bgimg" src="' + google_request_url + '">');
+    
+    // update greeting text
+    var location;
+    if (street && city) {
+      location = street + ", " + city;
+    }
+    else if (street) {
+      location = street;
+    }
+    else if (city) {
+      location = city;
+    }
+    $greeting.text("Current location: " + location)
 
     return false;
 };
--- readme.md
+++ readme.md
@@ -1,3 +1,5 @@
 # Udacity Ajax
 
-Udacity-Ajax is a fully complete, well documented version of the Udacity Ajax course's website project which uses the Google Maps, New York Times, and (unofficial) Wikipedia API.
+Udacity-Ajax is a fully complete, well documented version of the Udacity Ajax course's website project which uses the Google Maps API, New York Times API, and (unofficial) Wikipedia API.
+
+The code is highly commented so that viewers can quickly understand everything that is going on, even with minimal prior Ajax experience.
